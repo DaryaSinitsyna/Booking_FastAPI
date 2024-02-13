361cae38c372c58a114d00275c97c7e3a58b5bd0
Darya Sinitsyna <sinitsynadarya@mail.ru>
2024-02-13 12:32:48 +0300

    basic structure, models

---
+++ app/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, Query, Depends
+from datetime import date
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class HotelSearchArgs:
+    def __init__(
+            self,
+            location: str,
+            date_from: date,
+            date_to: date,
+            has_spa: bool = None,
+            stars: int = Query(None, ge=1, le=5),
+    ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.has_spa = has_spa
+        self.stars = stars
+
+
+@app.get("/hotels")
+def get_hotels(
+        search_args: HotelSearchArgs = Depends()
+):
+
+    return search_args
+
+
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+@app.post("/booking")
+def add_booking(booking: SBooking):
+    pass
